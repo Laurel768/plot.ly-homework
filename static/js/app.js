728bcd9e15643b84dbc459dc88d33512b79cde44
Laurel768 <laurel.whitten@hotmail.com>
2020-12-23 14:42:43 -0500

    add bar plot

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,39 @@
+function getPlots(id) {
+    d3.json("samples.json").then (sampledata =>{
+        console.log(sampledata)
+        var ids = sampledata.samples[0].otu_ids;
+        console.log(ids)
+        var sampleValues = sampledata.samples[0].sample_values.slice(0,10).reverse();
+        console.log(sampleValues)
+        var labels = sampledata.samples[0].otu_labels.slice(0,10);
+        console.log(labels)
+        var OTU_top = (sampledata.samples[0].otu_ids.slice(0,10)).reverse();
+        var OTU_id = OTU_top.map(d => "OTU " + d);
+        console.log(`OTU IDS: ${OTD_id}`)
+        var labels = sampledata.samples[0].otu_labels.slice(0,10);
+        console.log(`OTU_labels: ${labels}`)
+        var trace = {
+            x: sampleValues,
+            y: OTU_id,
+            text: labels,
+            marker: {
+            color: 'blue'},
+            type: "bar",
+            orientation: 'h',
+            };
+        var data = [trace];
+        var layout = {
+            title: "TOP 10 OTU",
+            yaxis:{
+                tickmode: "linear",
+            },
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 30
+            }
+        };    
+        
+Plotly.newPlot('bar', data, layout);
 
-
-function updateMetaData(data) {
-    var PANEL = document.getElementById("sample-metadata");
-    PANEL.innterHTML = '';
-    for(var key in data) {
-        h6tag = document.createElement("h6");
-        h6Text = document.createTextNode(`${key}: ${data[key]}`);
-        h6tag.append(h6tag);
-        PANEL.appendChild(h6tag);
-    }
-}
